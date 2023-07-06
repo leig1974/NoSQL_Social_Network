7cfad2b4d502d3520012136821b95f90a40883da
Leyla Callahan <leyla.callahan@yahoo.com>
2023-07-05 22:07:12 -0400

    commit

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/nosql-social-network-api",
+  // process.env.MONGODB_URI || "mongodb://mongodb://127.0.0.1:27017/nosql-social-network-api",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;
